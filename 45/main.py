2acd4ca37f03e3ff43226b26f4cfeee5811bed0a
segavvy <segavvy@yahoo.co.jp>
2016-12-03 15:31:16 +0900

    第5章45

---
+++ 45/main.py
@@ -0,0 +1,185 @@
+# coding: utf-8
+import CaboCha
+import re
+
+fname = 'neko.txt'
+fname_parsed = 'neko.txt.cabocha'
+fname_result = 'result.txt'
+
+
+def parse_neko():
+	'''「吾輩は猫である」を係り受け解析
+	「吾輩は猫である」(neko.txt)を係り受け解析してneko.txt.cabochaに保存する
+	'''
+	with open(fname) as data_file, \
+			open(fname_parsed, mode='w') as out_file:
+
+		cabocha = CaboCha.Parser()
+		for line in data_file:
+			out_file.write(
+				cabocha.parse(line).toString(CaboCha.FORMAT_LATTICE)
+			)
+
+
+class Morph:
+	'''
+	形態素クラス
+	表層形（surface）、基本形（base）、品詞（pos）、品詞細分類1（pos1）を
+	メンバー変数に持つ
+	'''
+	def __init__(self, surface, base, pos, pos1):
+		'''初期化'''
+		self.surface = surface
+		self.base = base
+		self.pos = pos
+		self.pos1 = pos1
+
+	def __str__(self):
+		'''オブジェクトの文字列表現'''
+		return 'surface[{}]\tbase[{}]\tpos[{}]\tpos1[{}]'\
+			.format(self.surface, self.base, self.pos, self.pos1)
+
+
+class Chunk:
+	'''
+	文節クラス
+	形態素（Morphオブジェクト）のリスト（morphs）、係り先文節インデックス番号（dst）、
+	係り元文節インデックス番号のリスト（srcs）をメンバー変数に持つ
+	'''
+
+	def __init__(self):
+		'''初期化'''
+		self.morphs = []
+		self.srcs = []
+		self.dst = -1
+
+	def __str__(self):
+		'''オブジェクトの文字列表現'''
+		surface = ''
+		for morph in self.morphs:
+			surface += morph.surface
+		return '{}\tsrcs{}\tdst[{}]'.format(surface, self.srcs, self.dst)
+
+	def normalized_surface(self):
+		'''句読点などの記号を除いた表層形'''
+		result = ''
+		for morph in self.morphs:
+			if morph.pos != '記号':
+				result += morph.surface
+		return result
+
+	def chk_pos(self, pos):
+		'''指定した品詞（pos）を含むかチェックする
+
+		戻り値：
+		品詞（pos）を含む場合はTrue
+		'''
+		for morph in self.morphs:
+			if morph.pos == pos:
+				return True
+		return False
+
+	def get_morphs_by_pos(self, pos):
+		'''指定した品詞（pos）の形態素のリストを返す
+
+		戻り値：
+		形態素（morph）のリスト、該当形態素がない場合は空のリスト
+		'''
+		return [res for res in self.morphs if res.pos == pos]
+
+
+def neco_lines():
+	'''「吾輩は猫である」の係り受け解析結果のジェネレータ
+	「吾輩は猫である」の係り受け解析結果を順次読み込んで、
+	1文ずつChunkクラスのリストを返す
+
+	戻り値：
+	1文のChunkクラスのリスト
+	'''
+	with open(fname_parsed) as file_parsed:
+
+		chunks = dict()		# idxをkeyにChunkを格納
+		idx = -1
+
+		for line in file_parsed:
+
+			# 1文の終了判定
+			if line == 'EOS\n':
+
+				# Chunkのリストを返す
+				if len(chunks) > 0:
+
+					# chunksをkeyでソートし、valueのみ取り出し
+					sorted_tuple = sorted(chunks.items(), key=lambda x: x[0])
+					yield list(zip(*sorted_tuple))[1]
+					chunks.clear()
+
+				else:
+					yield []
+
+			# 先頭が*の行は係り受け解析結果なので、Chunkを作成
+			elif line[0] == '*':
+
+				# Chunkのインデックス番号と係り先のインデックス番号取得
+				cols = line.split(' ')
+				idx = int(cols[1])
+				dst = int(re.search(r'(.*?)D', cols[2]).group(1))
+
+				# Chunkを生成（なければ）し、係り先のインデックス番号セット
+				if idx not in chunks:
+					chunks[idx] = Chunk()
+				chunks[idx].dst = dst
+
+				# 係り先のChunkを生成（なければ）し、係り元インデックス番号追加
+				if dst != -1:
+					if dst not in chunks:
+						chunks[dst] = Chunk()
+					chunks[dst].srcs.append(idx)
+
+			# それ以外の行は形態素解析結果なので、Morphを作りChunkに追加
+			else:
+
+				# 表層形はtab区切り、それ以外は','区切りでバラす
+				cols = line.split('\t')
+				res_cols = cols[1].split(',')
+
+				# Morph作成、リストに追加
+				chunks[idx].morphs.append(
+					Morph(
+						cols[0],		# surface
+						res_cols[6],	# base
+						res_cols[0],	# pos
+						res_cols[1]		# pos1
+					)
+				)
+
+		raise StopIteration
+
+
+# 係り受け解析
+parse_neko()
+
+# 結果ファイル作成
+with open(fname_result, mode='w') as out_file:
+
+	# 1文ずつリスト作成
+	for chunks in neco_lines():
+
+		# chunkを列挙
+		for chunk in chunks:
+
+			# 動詞を含むかチェック
+			verbs = chunk.get_morphs_by_pos('動詞')
+			if len(verbs) > 0:
+
+				# 係り元の助詞を列挙
+				prts = []
+				for src in chunk.srcs:
+					prts.extend(chunks[src].get_morphs_by_pos('助詞'))
+
+				# 係り元に助詞があれば結果として出力
+				if len(prts) > 0:
+					out_file.write('{}\t{}\n'.format(
+						verbs[0].base,		# 最左の動詞の基本系
+						' '.join(sorted(prt.surface for prt in prts))	# 助詞は辞書順
+					))
